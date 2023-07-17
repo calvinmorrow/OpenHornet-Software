bb798300691cca70067021358e5a783aa2c3a61f
jrsteensen <jrsteensen@users.noreply.github.com>
2023-07-17 22:20:05 +0000

    deploy: 8f93f6265acabf5e9a8a1b7d6a4395c47566ef2f

---
+++ navtreedata.js
@@ -0,0 +1,40 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "OpenHornet", "index.html", [
+    [ "README", "index.html", null ],
+    [ "Software Manual", "d7/d78/md__software_manual.html", null ],
+    [ "Style Guide", "d4/d46/md__2github_2workspace_2_s_t_y_l_e_g_u_i_d_e.html", null ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"d4/d46/md__2github_2workspace_2_s_t_y_l_e_g_u_i_d_e.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex0.js
@@ -0,0 +1,8 @@
+var NAVTREEINDEX0 =
+{
+"d4/d46/md__2github_2workspace_2_s_t_y_l_e_g_u_i_d_e.html":[2],
+"d7/d78/md__software_manual.html":[1],
+"index.html":[],
+"index.html":[0],
+"pages.html":[]
+};
